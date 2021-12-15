414112fc2fef037fbac372d7d483cf6a7d10f3f2
Markvitim <markvitim@gmail.com>
2021-12-15 15:03:58 +0300

    formated

--- src/BMIService.java
+++ src/BMIService.java
@@ -1,6 +1,6 @@
 public class BMIService {
     public float calculate(float weight, float height) {
-        float result = weight/ height/height;
+        float result = weight / height / height;
 
         return result;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-
 public class Main {
     public static void main(String[] args) {
         BMIService service = new BMIService();
-        float result = service.calculate(64f,1.64f);
+        float result = service.calculate(64f, 1.64f);
         System.out.println("Индекс массы тела " + result);
 
     }
